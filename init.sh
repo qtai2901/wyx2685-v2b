7c9ce7974b1256791f0d7d9763448a2ccdca5a9a
wyx2685 <wyx2685@gmail.com>
2023-11-30 20:15:33 +0900

    composer不检查php版本避免升级报错

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 rm -rf composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
-php composer.phar install -vvv
+php composer.phar install -vvv --ignore-platform-req=php
 php artisan v2board:install
 
 if [ -f "/etc/init.d/bt" ]; then
--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ git config --global --add safe.directory $(pwd)
 git fetch --all && git reset --hard origin/master && git pull origin master
 rm -rf composer.lock composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
-php composer.phar update -vvv
+php composer.phar update -vvv --ignore-platform-req=php
 php artisan v2board:update
 
 if [ -f "/etc/init.d/bt" ]; then
